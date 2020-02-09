7b1f47bba4f628261ffade8345f03fb441289d2c
Denise Manfredini <32367933+manfredinid@users.noreply.github.com>
2020-02-10 11:54:32 +1300

    Rename VECM - VEC2VAR.R to VAR/VECM - VEC2VAR.R

--- VAR/VECM - VEC2VAR.R
+++ VAR/VECM - VEC2VAR.R
@@ -8,28 +8,28 @@ library("pastecs")
 #Importar dados - Dados.csv
 Dados <- read.csv("Dados.csv", sep=";", dec=",")
 
-#Declarar série temporal
+#Declarar sÃ©rie temporal
 Dados <-ts(data = Dados, start = 1930, end = 1985, frequency = 1, deltat = 1)
 
-#matriz de dados endógenos
+#matriz de dados endÃ³genos
 dados <- data.frame(Dados[,c('TC0','CI0', 'TI0')])
 
-#vetor exógeno - dummy
+#vetor exÃ³geno - dummy
 exo <- data.frame(Dados[,c('dummyi')])
 
-#estatística descritiva dos dados endógenos
+#estatÃ­stica descritiva dos dados endÃ³genos
 
 stat <- stat.desc(dados)
 stat
 
 
 
-#seleção da ordem do VAR (k)
+#seleÃ§Ã£o da ordem do VAR (k)
 CDI<-VARselect (log(dados), type="const", lag.max = 05, exogen=exo)$selection
 CDI
 
 
-#teste de Johansen - vetores de cointegração
+#teste de Johansen - vetores de cointegraÃ§Ã£o
 john <-ca.jo(log(dados),  type = c("trace"), ecdet="none", K=2, spec = "transitory", dumvar=exo)
 
 if (john@teststat[1]<john@cval[, "1pct"][3]){
@@ -49,18 +49,18 @@ vecm.r1 <- cajorls(john, r = 1)
 summary(vecm.r1$rlm)
 vecm.r1$beta
 
-#VECM representado como VAR em níveis
+#VECM representado como VAR em nÃ­veis
 
 vecm.level <- vec2var(john, r=1)
   
 
-# teste de autocorrelação
+# teste de autocorrelaÃ§Ã£o
 arch.test(vecm.level)
 
 
 #teste de hetercedasticidade 
 serial.test(vecm.level, type = c("PT.asymptotic"))
 
-#Decomposição da variância
+#DecomposiÃ§Ã£o da variÃ¢ncia
 fevd <- fevd(vecm.level)
 fevd
